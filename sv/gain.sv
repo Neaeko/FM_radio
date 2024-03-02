8a6bcf44b9d83e4ee501ba5f551b0b94a06fe885
someone <haha@haha.com>
2024-03-02 15:02:55 -0600

    gain: update gain

--- sv/gain.sv
+++ sv/gain.sv
@@ -1,23 +1,28 @@
-module fir_complex_top#(
+module gain_32_mono#(
     parameter GAIN = 1
 )(
-    input  logic clock,
-    input  logic reset,
-
-    input  logic [31:0] left_in_dout,
-    input  logic [31:0] right_in_dout,
-
-    output  logic in_full,
-    input   logic in_wr_en,
-
-    output  logic [31:0] right_out_din,
-    output  logic [31:0] left_out_din,
-    input   logic out_rd_en,
-    output  logic out_empty
-
+    input  logic [31:0] in_dout,
+    output  logic [31:0] out_din
 );
 
 
+always_comb begin
+    out_din = mul_frac10_32b(in_dout, GAIN) << (14-10);
+end
 
+
+// fixed point multiplication
+function automatic logic [31:0] mul_frac10_32b (
+    input logic [31:0] ina,
+    input logic [31:0] inb
+);
+    // Perform the multiplication
+    logic [63:0] product = $signed(ina) * $signed(inb);
+
+    // Shift the product right by 10 bits to maintain the 10-bit fractional part
+    logic [31:0] result = product >> 10;
+
+    return result;
+endfunction
 
 endmodule
