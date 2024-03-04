472c20f4ea7ee5ace855c6afe61a1080ef68409e
someone <haha@haha.com>
2024-03-04 12:21:00 -0600

    change dequant

--- sv/functions.svh
+++ sv/functions.svh
@@ -6,8 +6,8 @@ function logic[31:0] DEQUANTIZE;
 input logic[31:0] i;
     begin
 	logic [31:0] result;
-        //return int'($signed(i) / $signed(1 << 10));
-	result = (i[31] == 1'b1) ? (($signed(i) + (1 << 10) - 1) >> 10) : (i >> 10);
+        result =  ($signed(i) / $signed(1 << 10));
+	
 	return result;
     end
 endfunction
@@ -22,7 +22,7 @@ function automatic logic [31:0] mul_frac10_32b (
     logic [63:0] product = $signed(ina) * $signed(inb);
 
     // Shift the product right by 10 bits to maintain the 10-bit fractional part
-    logic [31:0] result = product >> 10;
+    logic [31:0] result = DEQUANTIZE(products);
     
 
     return result;
